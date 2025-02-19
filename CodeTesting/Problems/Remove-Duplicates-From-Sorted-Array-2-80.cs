b58bb72f17fa618ed97cf1c548e3e8256188a4e4
Hung <hungtruong19999@gmail.com>
2025-02-18 21:09:20 -0600

    Problem 80

---
+++ CodeTesting/Problems/Remove-Duplicates-From-Sorted-Array-2-80.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodePractice.Problems
+{
+    class Remove_Duplicates_From_Sorted_Array_2_80
+    {
+
+        /// <summary>
+        /// My Initial Solution
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int RemoveDuplicates(int[] nums)
+        {
+            int p1 = 0;
+            int dupCounter = 0;
+
+            for(int p2 = 0; p2 < nums.Length;p2++)
+            {
+                if(p2 == 5)
+                {
+                    Console.WriteLine(p2);
+                }
+                
+                if(dupCounter == 2 && nums[p2] != nums[p1 - 1])
+                {
+                    dupCounter = 0;
+                }
+
+                if(dupCounter == 1 && nums[p2] != nums[p2 - 1])
+                {
+                    dupCounter = 0;
+                }
+
+                if(nums[p2] >= nums[p1] && dupCounter < 2)
+                {
+                    nums[p1] = nums[p2];
+                    p1++;
+                    dupCounter++;
+                }
+            }
+
+            return p1;
+        }
+
+        /// <summary>
+        /// Improved Space and Time Complexity
+        /// Understand that Because we are allowing two values, we can check pointer1 - 2 to see if they are the same
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int ImpovedRemoveDuplicates(int[] nums)
+        {
+            if (nums.Length <= 2) return nums.Length;
+
+            int p1 = 2;
+
+            for (int p2 = 2; p2 < nums.Length; p2++)
+            {
+                if(nums[p2] != nums[p1 - 2])
+                {
+                    nums[p1] = nums[p2];
+                    p1++;
+                }
+            }
+
+            return p1;
+        }
+    }
+}
--- CodeTesting/Program.cs
+++ CodeTesting/Program.cs
@@ -7,11 +7,11 @@ namespace CodeTesting
     {
         static void Main(string[] args)
         {
-            int[] nums = new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+            int[] nums = new int[] { 0, 0, 1, 1, 1, 1, 2, 3, 3};
 
-            Remove_Duplicates_From_Sorted_Array_26 problem26 = new Remove_Duplicates_From_Sorted_Array_26();
+            Remove_Duplicates_From_Sorted_Array_2_80 problem = new Remove_Duplicates_From_Sorted_Array_2_80();
 
-            problem26.RemoveDuplicates(nums);
+            problem.RemoveDuplicates(nums);
         }
     }
 }
