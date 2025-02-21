17bab74667b470dc39ed814f98e725c03fa531aa
Hung <hungtruong19999@gmail.com>
2025-02-21 01:20:16 -0600

    Problem 169

---
+++ CodeTesting/Problems/MajorityElement-169.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodePractice.Problems
+{
+    class MajorityElement_169
+    {
+        public int MajorityElement(int[] nums)
+        {
+            int majority = Math.Abs(nums.Length / 2);
+            Dictionary<int, int> keyValuePairs = new Dictionary<int, int>();
+
+            foreach (var item in nums)
+            {
+                if(keyValuePairs.ContainsKey(item))
+                {
+                    keyValuePairs[item]++;
+                    if (keyValuePairs[item] > majority)
+                    {
+                        return item;
+                    }
+                }
+                else
+                {
+                    keyValuePairs[item] = 1;
+                }
+            }
+
+            return 0;
+        }
+    }
+}
