111b4c37884e754fabc4a7d53a0426ae11329bfe
Hung <hungtruong19999@gmail.com>
2025-02-26 01:55:42 -0600

    Least Common Prefix

---
+++ CodeTesting/Problems/Longest-common-prefix-14.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodePractice.Problems
+{
+    class Longest_common_prefix_14
+    {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            if (strs == null || strs.Length == 0) return "";
+
+            string prefix = strs[0];
+
+            for(int i = 1; i < strs.Length; i++)
+            {
+                while(strs[i].IndexOf(prefix) != 0)
+                {
+                    prefix = prefix.Substring(0, prefix.Length - 1);
+                    if (prefix == "") return "";
+                }
+            }
+            return prefix;
+        }
+    }
+}
