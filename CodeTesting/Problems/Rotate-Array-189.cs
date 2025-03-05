64be10e6ef21d779959fa4f9670bccf571c3fc17
Hung <hungtruong19999@gmail.com>
2025-03-04 21:17:46 -0600

    Rotate array 189

---
+++ CodeTesting/Problems/Rotate-Array-189.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodePractice.Problems
+{
+    class Rotate_Array_189
+    {
+        //Idea
+        //Use two pointers,
+        //Iterate through the array while it is less then second pointer
+        public void Rotate(int[] nums,int k)
+        {
+            if (nums == null || nums.Length == 0 || k <= 0) return;
+
+            //if length is 3, k = 2 is the same as k = 5
+            k = k % nums.Length;
+
+            if (k == 0) return;
+
+            //Idea, filp the entire array
+            //[1,2,3,4,5] = [5,4,3,2,1]
+            //One thing to notice is that this way, the end of the array is at the beginning and the end of the array is at the end
+            reverseHelper(nums, 0, nums.Length - 1);
+
+            //Now reverse the beginning of the array
+            //[4,5,3,2,1]
+            reverseHelper(nums, 0, k - 1);
+
+            //Now reverse the end of the array
+            //[4,5,1,2,3]
+            reverseHelper(nums, k, nums.Length - 1);
+
+        }
+
+        private void reverseHelper(int[] nums, int start, int end)
+        {
+            while(start < end)
+            {
+                int temp = nums[start];
+                nums[start] = nums[end];
+                nums[end] = temp;
+                start++;
+                end--;
+            }
+        }
+    }
+}
--- CodeTesting/Program.cs
+++ CodeTesting/Program.cs
@@ -7,16 +7,16 @@ namespace CodeTesting
     {
         static void Main(string[] args)
         {
-            int[] nums = new int[] { 7, 1, 5, 3, 7, 4 };
+            int[] nums = new int[] {1, 2, 3, 4, 5, 6, 7};
             int[] nums2 = new int[] { 2, 4, 1 };
             int[] prices = new int[] {7, 1, 5, 3, 6, 4};
 
             string s = "a ";
             string haystack = "sadbutsad", needle = "sad";
 
-            Best_Time_To_Buy_and_Sell_Stock_122 problem= new Best_Time_To_Buy_and_Sell_Stock_122();
+            Rotate_Array_189 problem= new Rotate_Array_189();
 
-            problem.MaxProfit_2(prices);
+            problem.Rotate(nums,3);
         }
     }
 }
