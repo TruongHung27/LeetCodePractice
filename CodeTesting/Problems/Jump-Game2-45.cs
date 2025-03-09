78d0e1c0f4ca4dda8a95e3eed39f5ccfbb8e34ed
Hung <hungtruong19999@gmail.com>
2025-03-08 21:32:41 -0600

    Problem 45

---
+++ CodeTesting/Problems/Jump-Game2-45.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodePractice.Problems
+{
+    class Jump_Game2
+    {
+        public int Jump(int[] nums)
+        {
+            if (nums.Length <= 1) return 0;
+
+            int jumps = 0;
+            int currentMaxReach = 0;
+            int nextMaxReach = 0;
+
+            for(int i = 0; i < nums.Length - 1; i++)
+            {
+                nextMaxReach = Math.Max(nextMaxReach, i + nums[i]);
+
+                if(i == currentMaxReach)
+                {
+                    jumps++;
+
+                    currentMaxReach = nextMaxReach;
+
+                    if(currentMaxReach >= nums.Length - 1)
+                    {
+                        return jumps;
+                    }
+                }
+            }
+            return jumps;
+        }
+    }
+}
--- CodeTesting/Program.cs
+++ CodeTesting/Program.cs
@@ -7,16 +7,16 @@ namespace CodeTesting
     {
         static void Main(string[] args)
         {
-            int[] nums = new int[] {2, 3, 1, 1, 4};
+            int[] nums = new int[] {2, 3, 0,1,4};
             int[] nums2 = new int[] { 2, 4, 1 };
             int[] prices = new int[] {7, 1, 5, 3, 6, 4};
 
             string s = "a ";
             string haystack = "sadbutsad", needle = "sad";
 
-            Jump_Game_55 problem= new Jump_Game_55();
+            Jump_Game2 problem= new Jump_Game2();
 
-            problem.CanJump(nums);
+            problem.Jump(nums);
         }
     }
 }
