0ee5f5d2fb6848da37685123a08c5899fbb000b0
Hung <hungtruong19999@gmail.com>
2025-03-12 01:19:43 -0500

    Probelm 380

---
+++ CodeTesting/Problems/Insert-Delete-Random-0(1)-380.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodePractice.Problems
+{
+    class Insert_Delete_Random_0_1__380
+    {
+        public class RandomizeSet
+        {
+            List<int> randomizeSet;
+            public RandomizeSet()
+            {
+                randomizeSet = new List<int>();
+            }
+
+
+            public bool Insert(int val)
+            {
+                if(!randomizeSet.Contains(val))
+                {
+                    randomizeSet.Add(val);
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            public bool Remove(int val)
+            {
+                if(randomizeSet.Contains(val))
+                {
+                    randomizeSet.Remove(val);
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            public int GetRandom()
+            {
+                Random random = new Random();
+                int randomIndex = random.Next(randomizeSet.Count);
+                return randomizeSet[randomIndex];
+            }
+
+        }
+    }
+}
