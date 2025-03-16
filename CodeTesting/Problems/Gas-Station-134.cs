8fe1fea3c674dc320b33ce912d0b05f75bf498d2
Hung <hungtruong19999@gmail.com>
2025-03-16 18:23:06 -0500

    Problem 134.
    Two solutions

---
+++ CodeTesting/Problems/Gas-Station-134.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodePractice.Problems
+{
+    class Gas_Station_134
+    {
+        public int CanCompleteCircuit(int[] gas, int[] cost)
+        {
+            if (gas.Length != cost.Length) return -1;
+
+            int totalGas = 0;
+            int totalCost = 0;
+            int currentGas = 0;
+            int startIndex = 0;
+
+            for (int i = 0; i < gas.Length; i++)
+            {
+                totalGas += gas[i];
+                totalCost += cost[i];
+
+                currentGas += gas[i] - cost[i];
+                if (currentGas < 0)
+                {
+                    startIndex = i + 1;
+                    currentGas = 0;
+                }
+            }
+
+            // If total gas is less than total cost, no solution exists
+            return totalGas >= totalCost ? startIndex : -1;
+
+
+
+            //Logic works but is O(n^2) and too slow
+
+            //if (gas.Length > Math.Pow(10, 4) || cost.Length > Math.Pow(10, 4)) return -1;
+            //if (gas.Length != cost.Length) return -1;
+
+
+            //for (int j = 0; j < gas.Length; j++)
+            //{
+            //    int gasTank = 0;
+            //    bool canComplete = true;
+
+            //    for (int i = 0; i < gas.Length; i++)
+            //    {
+            //        int currentIndex = (j + i) % gas.Length;
+            //        gasTank += gas[currentIndex];
+
+            //        if(gasTank < cost[currentIndex])
+            //        {
+            //            canComplete = false;
+            //            break;
+            //        }
+
+            //        gasTank -= cost[currentIndex];
+            //    }
+
+            //    if (canComplete)
+            //    {
+            //        return j;
+            //    }
+
+            //}
+            //return -1;
+        }
+    }
+}
--- CodeTesting/Program.cs
+++ CodeTesting/Program.cs
@@ -7,19 +7,19 @@ namespace CodeTesting
     {
         static void Main(string[] args)
         {
-            int[] nums = new int[] {1,2,3,4};
-            int[] nums2 = new int[] { -1,1,0,-3,3};
+            int[] nums = new int[] {1,2,3,4,5};
+            int[] nums2 = new int[] { 3,4,5,1,2};
             int[] prices = new int[] {7, 1, 5, 3, 6, 4};
             int[] ciations = new int[] { 3, 0, 6, 1, 5 };
 
             string s = "a ";
             string haystack = "sadbutsad", needle = "sad";
 
-            Product_Of_Array_Except_Self_238 problem = new Product_Of_Array_Except_Self_238();
+            Gas_Station_134 problem = new Gas_Station_134();
 
             int i = 3749;
 
-            problem.ProductExceptSelf(nums2);
+            problem.CanCompleteCircuit(nums,nums2);
         }
     }
 }
