9c042865eec95fd63c3d4a04d66bba1fc0101ffa
Hung <hungtruong19999@gmail.com>
2025-03-16 01:32:41 -0500

    Problem 238

---
+++ CodeTesting/Problems/Product-Of-Array-Except-Self-238.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodePractice.Problems
+{
+    class Product_Of_Array_Except_Self_238
+    {
+        public int[] ProductExceptSelf(int[] nums)
+        {
+            int[] result = new int[nums.Length];
+
+            int leftProduct = 1;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                result[i] = leftProduct;
+                leftProduct *= nums[i];
+            }
+
+            int rightProduct = 1;
+            for (int i = nums.Length; i >= 0; i--)
+            {
+                result[i] *= rightProduct;
+                rightProduct *= nums[i];
+            }
+
+            return result;
+        }
+    }
+}
--- CodeTesting/Program.cs
+++ CodeTesting/Program.cs
@@ -7,19 +7,19 @@ namespace CodeTesting
     {
         static void Main(string[] args)
         {
-            int[] nums = new int[] {2, 3, 0,1,4};
-            int[] nums2 = new int[] { 2, 4, 1 };
+            int[] nums = new int[] {1,2,3,4};
+            int[] nums2 = new int[] { -1,1,0,-3,3};
             int[] prices = new int[] {7, 1, 5, 3, 6, 4};
             int[] ciations = new int[] { 3, 0, 6, 1, 5 };
 
             string s = "a ";
             string haystack = "sadbutsad", needle = "sad";
 
-            Integer_To_Roman_12 problem = new Integer_To_Roman_12();
+            Product_Of_Array_Except_Self_238 problem = new Product_Of_Array_Except_Self_238();
 
             int i = 3749;
 
-            problem.IntToRoman(i);
+            problem.ProductExceptSelf(nums2);
         }
     }
 }
