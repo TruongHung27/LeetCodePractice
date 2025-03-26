8dff89fc5da7cc867e4b8a6ca5e806bab259957d
Hung <hungtruong19999@gmail.com>
2025-03-26 01:07:58 -0500

    Problem 392

---
+++ CodeTesting/Problems/Is-Subsequence-392.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodePractice.Problems
+{
+    class Is_Subsequence_392
+    {
+        public bool IsSubsequence(string s, string t)
+        {
+
+            if (string.IsNullOrEmpty(s)) return true;
+            if (string.IsNullOrEmpty(t)) return false;
+
+            int sPointer = 0;
+
+            for (int i = 0; i < t.Length; i++)
+            {
+                if (s[sPointer] == t[i])
+                {
+                    sPointer++;
+
+                    if (sPointer == s.Length)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+        
+    }
+}
