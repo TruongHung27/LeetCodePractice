7b9aa5f225ce82ed43f7e9470ada6e6fa4ee9d34
Hung <hungtruong19999@gmail.com>
2025-03-27 01:39:33 -0500

    Problem 205

---
+++ CodeTesting/Problems/Isomorphic-Strings-205.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodePractice.Problems
+{
+    class Isomorphic_Strings_205
+    {
+        public bool IsIsomorphic(string s, string t)
+        {
+            Dictionary<char, char> sValuePairs = new Dictionary<char, char>();
+            Dictionary<char, char> tValuePairs = new Dictionary<char, char>();
+
+            if(s.Length == t.Length)
+            {
+                for (int i = 0; i < s.Length; i++)
+                {
+                    var sChar = s[i];
+                    var tChar = t[i];
+
+                    if(sValuePairs.ContainsKey(sChar))
+                    {
+                        if (sValuePairs[sChar] != tChar)
+                        {
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        sValuePairs[sChar] = tChar;
+                    }
+
+                    if(tValuePairs.ContainsKey(tChar))
+                    {
+                        if(tValuePairs[tChar] != sChar)
+                        {
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        tValuePairs[tChar] = sChar;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
